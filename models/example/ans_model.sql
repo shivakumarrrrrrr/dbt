e065dcd2ab3a0237b6fa207567574229589e773e
Shiva Kumar <shiva.kumar1214.sk@gmail.com>
2021-10-31 09:10:34 +0000

    highest_fatality_rate

--- models/example/ans_model.sql
+++ models/example/ans_model.sql
@@ -5,6 +5,7 @@ Answer model
 */
 
 
+{{ config(materialized='table') }}
 with source_data as (
 select *
 from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_SHIVA_KUMAR"
--- models/example/mom_increase.sql
+++ models/example/mom_increase.sql
@@ -1,8 +1,34 @@
+{{ config(materialized='table') }}
+with source_data as (
+select *
+from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_SHIVA_KUMAR"
+),
+
+geo_attributes as (
+select _ROW, TIME_ZONE,LATITUDE,LONGITUDE,LOCATION_ISO_CODE,
+TOTAL_REGENCIES,
+POPULATION,POPULATION_DENSITY,TOTAL_CITIES,TOTAL_DISTRICTS,ISLAND,
+PROVINCE,SPECIAL_STATUS,CONTINENT,TOTAL_URBAN_VILLAGES,TOTAL_RURAL_VILLAGES,COUNTRY,
+LOCATION_LEVEL,AREA_KM_2_,LOCATION
+from source_data
+),
+
+covid_case_details as (
+select _ROW, TOTAL_RECOVERED,TOTAL_CASES_PER_MILLION,
+GROWTH_FACTOR_OF_NEW_DEATHS,TOTAL_ACTIVE_CASES,
+NEW_CASES_PER_MILLION,NEW_DEATHS_PER_MILLION,
+NEW_RECOVERED,CASE_FATALITY_RATE,NEW_DEATHS,
+NEW_CASES,TOTAL_DEATHS,cast(DATE as date) as DATE_,CASE_RECOVERED_RATE,
+NEW_ACTIVE_CASES,TOTAL_CASES,TOTAL_DEATHS_PER_MILLION,
+GROWTH_FACTOR_OF_NEW_CASES
+
+from source_data
+)
 
 
-select MONTH(DATE_) as month_int,
-monthname(DATE_) as for_month,
-sum(TOTAL_CASES) as tot_monthly_cases
-from {{ ref('ans_model.covid_case_details') }}
-group by for_month, month_int
-order by month_int 
+select  MONTH(DATE_) month_mm,
+        sum(TOTAL_CASES) as tot_monthly_cases
+from covid_case_details
+where year(date_) = 2020
+group by month_mm
+order by month_mm 
